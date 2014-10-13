8c71526d4fb8da41551fd34688369b2007d999a8
ludo1026 <ludovic.chaboud@sogeti.com>
2014-10-13 12:50:41 +0200

    Database update

---
+++ README.md
@@ -0,0 +1,10 @@
+Update Cassandra and Solr
+=========================
+
+* In the folder "cassandra", copy files from the folder "apispark\modules\com.apispark.model.cell\resources\cassandra".
+
+* In the folder "solr", copy files from the folder : "apispark\modules\com.apispark.service.search\resources".
+
+* In the "update.sh", define the variable "cassandra_home".
+
+* Run ```./update.sh```
---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x #echo on
+
+cassandra_home=/opt/dse/dse-4.5.2
+
+echo "update cassandra"
+$cassandra_home/bin/cqlsh -f other/apispark-keyspace-drop.txt
+$cassandra_home/bin/cqlsh -f other/apispark-keyspace-create.txt
+$cassandra_home/bin/cqlsh -f cassandra/apispark-keyspace-cql.txt
+$cassandra_home/bin/cqlsh -f cassandra/apispark-keyspace-data.txt
+
+echo "update solr"
+curl http://localhost:8983/solr/resource/apispark.DashboardView/solrconfig.xml --data-binary @solr/solrconfig.xml -H 'Content-type:text/xml; charset=utf-8'
+curl http://localhost:8983/solr/resource/apispark.DashboardView/schema.xml --data-binary @solr/schema.xml -H 'Content-type:text/xml; charset=utf-8'
+curl http://localhost:8983/solr/resource/apispark.DashboardView/stopwords.txt --data-binary @solr/stopwords.txt -H 'Content-type:text/xml; charset=utf-8'
+curl http://localhost:8983/solr/resource/apispark.DashboardView/synonyms.txt --data-binary @solr/synonyms.txt -H 'Content-type:text/xml; charset=utf-8'
+curl "http://localhost:8983/solr/admin/cores?action=CREATE&name=apispark.DashboardView"
+
+echo "users"
+$cassandra_home/bin/cqlsh -f other/user.txt
